7fd6c13a5a6484f050bd2ffa5295128ac7247999
setaya123 <setaya.soleimani26@ycdsbk12.ca>
2025-01-11 20:18:00 -0500

    Added the main menu

--- Culminating.java
+++ Culminating.java
@@ -1,4 +1,4 @@
-import arc.*;
+import arc.*; 
 import java.util.Random;
 public class Culminating{
 	
@@ -11,22 +11,33 @@ public class Culminating{
 		con.println(" Enter your name: ");
 		strUserName=con.readLine();
 		
-		con.println(" Would you like to play a quiz?(1 for Yes, any other number for No):");
-		int intChoice=con.readInt();
+		String strMenuChoice="";
+		while(!strMenuChoice.equalsIgnoreCase("q")){
+			con.println(" Main Menu: ");
+			con.println(" Play quizzes (p)  ");
+			con.println(" View High Score (v) ");
+			con.println(" Quit (q) ");
+			con.println(" Enter your choice" );
+			strMenuChoice=con.readLine();
 		
-		if(intChoice==1){
+		if(strMenuChoice.equalsIgnoreCase("p")){
 			showAvailableQuizzes(con);
 			con.println(" Enter your choice of the quiz: ");
 			strQuizChoice=con.readLine();
 		
 		if(strQuizChoice.equalsIgnoreCase("kia")||strQuizChoice.equalsIgnoreCase("toyoto") || strQuizChoice.equalsIgnoreCase("rolls-royce")){
-			playQuizzes(con,strUsername,strQuizChoice);
+			 playQuizzes(con, strUserName, strQuizChoice);
 		}else{
 			con.println(" Invalid quiz choice. Returning to main menu.");
 		}
-	}else{
+	} else if (strMenuChoice.equalsIgnoreCase("v")) {
+    recordedHighScores(con);
+	}else if(strMenuChoice.equalsIgnoreCase("q")){
 		con.println(" Thank you for playing Multiple Choice Game! ");	
+	}else{
+		con.println("Invalid Choice. Please select one of three above options");
 	}
+}
 }
 public static void showAvailableQuizzes(Console con){		  
 		String strLine;
@@ -39,7 +50,7 @@ public static void showAvailableQuizzes(Console con){
         quizNames.close();
     }
 	  
-	  public static void playQuizzes(Console con, String strUsername, String strQuizChoice){
+    public static void playQuizzes(Console con, String strUserName, String strQuizChoice) {
 		  TextInputFile quizFile=new TextInputFile(strQuizChoice.toLowerCase()+".txt");
 		  
 		  int intLines=0;
@@ -64,6 +75,7 @@ public static void showAvailableQuizzes(Console con){
 			quizParts[i][4]=quizFile.readLine();
 			quizParts[i][5]=quizFile.readLine();
 			randomNumbers[i]=(int)(Math.random()*100)+1;
+
 		}
 		quizFile.close();
 		
@@ -93,7 +105,18 @@ public static void showAvailableQuizzes(Console con){
 			    con.println(" Enter your answer(a,b,c,d): ");
 			    String strAnswer=con.readLine();
 			    
-			 if(strAnswer.equalsIgnoreCase(quizParts[i][5])){
+			int intuserAnswer=0;
+			
+			if(strAnswer.equalsIgnoreCase("a")){
+				 intuserAnswer=1;
+		    }else if(strAnswer.equalsIgnoreCase("b")){
+				intuserAnswer=2;
+			}else if(strAnswer.equalsIgnoreCase("c")){
+				intuserAnswer=3;
+			}else if(strAnswer.equalsIgnoreCase("d")){
+				intuserAnswer=4;
+			
+			if(intuserAnswer==Integer.parseInt(quizParts[i][5])){
 				 con.println(" Correct" );
 				 intCorrect=intCorrect+1;
 			}else{
@@ -102,20 +125,29 @@ public static void showAvailableQuizzes(Console con){
 		}
 		
 		double dblPercentage=(intCorrect/intAmount)*100;
-        highScore(con, strUserName, strQuizChoice, intCorrect, intAmount, dblPercentage);
-        
+        highScores(con, strUserName, strQuizChoice, intCorrect, intAmount, dblPercentage);
+    }
+}
+    public static void recordedHighScores(Console con){
+		TextInputFile highScores=new TextInputFile("HighScores.txt");
+		String strReadScore;
+		con.println(" High Scores : ");
+		while(highScores.eof()==false){
+			strReadScore=highScores.readLine();
+			con.println(strReadScore);
+		}
+		highScores.close();
 	}
-    public static void highScore(Console con, String strUserName, String strQuizChoice, int intCorrect, int intAmount, double dblPercentage) {
-		con.println(" Quiz complete!");
-        con.println("User: " + strUserName);
+		
+    public static void highScores(Console con, String strUserName, String strQuizChoice, int intCorrect, int intAmount, double dblPercentage) {
+        con.println("User: " + strUserName);  
         con.println("Quiz: " + strQuizChoice);
         con.println("Score: " + intCorrect + "/" + intAmount + " (" + dblPercentage + "%)");
         TextOutputFile highScores = new TextOutputFile("HighScores.txt", true);
         highScores.println(strUserName + " - " + strQuizChoice + " - " + dblPercentage + "%");
         highScores.close();
-	}
-	}
-
+    }
+}
 
 				
 		 
