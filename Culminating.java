2e1469eaa2149cf43dc8e110765c198449e68e68
setaya123 <setaya.soleimani26@ycdsbk12.ca>
2025-01-13 21:47:05 -0500

    Sorting HighScores
    Sorted HighScores with Selection Sort. Might change approach(have not fully grasped Selection Sort).

--- Culminating.java
+++ Culminating.java
@@ -10,20 +10,24 @@ public class Culminating{
 		con.println(" Welcome to the Multiple-Choice Game!");
 		con.println(" Enter your name: ");
 		strUserName=con.readLine();
+		con.println();
 		
 		String strMenuChoice="";
 		while(!strMenuChoice.equalsIgnoreCase("q")){
 			con.println(" Main Menu: ");
 			con.println(" Play quizzes (p)  ");
 			con.println(" View High Score (v) ");
-			con.println(" Quit (q) ");
+			con.println(" Quit Game (q) ");
 			con.println(" Enter your choice" );
 			strMenuChoice=con.readLine();
+			con.println();
 		
 		if(strMenuChoice.equalsIgnoreCase("p")){
 			showAvailableQuizzes(con);
+			con.println();
 			con.println(" Enter your choice of the quiz: ");
 			strQuizChoice=con.readLine();
+			con.println();
 		
 		if(strQuizChoice.equalsIgnoreCase("kia")||strQuizChoice.equalsIgnoreCase("toyoto") || strQuizChoice.equalsIgnoreCase("rolls-royce")){
 			 playQuizzes(con, strUserName, strQuizChoice);
@@ -34,14 +38,16 @@ public class Culminating{
     recordedHighScores(con);
 	}else if(strMenuChoice.equalsIgnoreCase("q")){
 		con.println(" Thank you for playing Multiple Choice Game! ");	
+	    con.println();
 	}else{
 		con.println("Invalid Choice. Please select one of three above options");
-	}
+	    con.println();
+	 }
 }
 }
 public static void showAvailableQuizzes(Console con){		  
 		String strLine;
-		TextInputFile quizNames=new TextInputFile("QuizNames.txt");
+		TextInputFile quizNames=new TextInputFile("tests.txt");
 	    con.println(" Avaliable Quizzes: ");
 	    while(quizNames.eof()==false){
 		strLine=quizNames.readLine();
@@ -67,15 +73,16 @@ public static void showAvailableQuizzes(Console con){
 		Random rand=new Random();
 		
 		quizFile=new TextInputFile(strQuizChoice.toLowerCase()+".txt");
-		for(int i=0;i<intAmount;i++){
-			quizParts[i][0]=quizFile.readLine();
-			quizParts[i][1]=quizFile.readLine();
-			quizParts[i][2]=quizFile.readLine();
-			quizParts[i][3]=quizFile.readLine();
-			quizParts[i][4]=quizFile.readLine();
-			quizParts[i][5]=quizFile.readLine();
-			randomNumbers[i]=(int)(Math.random()*100)+1;
-			quizParts[i][6]=""+randomNumbers[i];
+		int intCount;
+		for(intCount=0;intCount<intAmount;intCount++){
+			quizParts[intCount][0]=quizFile.readLine();
+			quizParts[intCount][1]=quizFile.readLine();
+			quizParts[intCount][2]=quizFile.readLine();
+			quizParts[intCount][3]=quizFile.readLine();
+			quizParts[intCount][4]=quizFile.readLine();
+			quizParts[intCount][5]=quizFile.readLine();
+			randomNumbers[intCount]=(int)(Math.random()*100)+1;
+			quizParts[intCount][6]=""+randomNumbers[intCount];
 
 		}
 		quizFile.close();
@@ -98,14 +105,14 @@ public static void showAvailableQuizzes(Console con){
 	}
 	        int intCorrect=0;
 	        double dblPercentage=0;
-
-	        for(int i=0; i<intAmount; i++){
+            
+	        for(intCount=0; intCount<intAmount; intCount++){
 			    highScores(con, strUserName, strQuizChoice, intCorrect, intAmount, dblPercentage);
-				con.println(" Question: " + quizParts[i][0]);
-				con.println(" a) " + quizParts[i][1]);
-				con.println(" b) " + quizParts[i][2]);
-				con.println(" c) " + quizParts[i][3]);
-				con.println(" d) " + quizParts[i][4]);	
+				con.println(" Question: " + quizParts[intCount][0]);
+				con.println(" a) " + quizParts[intCount][1]);
+				con.println(" b) " + quizParts[intCount][2]);
+				con.println(" c) " + quizParts[intCount][3]);
+				con.println(" d) " + quizParts[intCount][4]);	
 			    con.println(" Enter your answer(a,b,c,d): ");
 			    String strAnswer=con.readLine();
 			    con.println();
@@ -122,12 +129,12 @@ public static void showAvailableQuizzes(Console con){
 				intuserAnswer=4;
 			}
 			
-			if(quizParts[i][intuserAnswer].equals(quizParts[i][5])){
+			if(quizParts[intCount][intuserAnswer].equals(quizParts[intCount][5])){
 				 con.println(" Correct" );
 				 con.println();
 				 intCorrect=intCorrect+1;
 			}else{
-				con.println("Wrong. The correct answer was: " + quizParts[i][5]);
+				con.println("Wrong. The correct answer was: " + quizParts[intCount][5]);
 				con.println();
 		    }
 		    dblPercentage=((double)intCorrect/(double)intAmount)*100;
@@ -139,24 +146,54 @@ public static void showAvailableQuizzes(Console con){
 			con.println(" Quiz: " + strQuizChoice);
 			con.println(" Score:" + intCorrect + "/" + intAmount);
 			con.println(" Percentage: " + dblPercentage + "%");
+			writeScore(strUserName, strQuizChoice,dblPercentage);
 		}else{
 			con.println(" Nice Try! You did not pass the quiz. ");
 			con.println(" Quiz: " + strQuizChoice);
 			con.println(" Score: " + intCorrect + "/" + intAmount);
-			con.println("Percentage: " + dblPercentage + "%");
+			con.println("Percentage: " + dblPercentage + "%");			
+			writeScore(strUserName, strQuizChoice,dblPercentage);
+
 		}
-			
+	     con.println();
 		
 }
     public static void recordedHighScores(Console con){
 		TextInputFile highScores=new TextInputFile("HighScores.txt");
 		String strReadScore;
 		con.println(" High Scores : ");
+		String strdata="";
+		int intCount=0;
+		
 		while(highScores.eof()==false){
 			strReadScore=highScores.readLine();
-			con.println(strReadScore);
+		    strdata=strdata + strReadScore + ":";
+		    intCount=intCount + 1; // to know how many rows there are
 		}
 		highScores.close();
+		String dataarr[]=strdata.split(":"); //put in one line because 
+		for(int i=0;i<intCount;i++){
+			String strrowData[]=dataarr[i].split(" - ");
+			String strp1=strrowData[2];
+			strp1=strp1.substring(0,strp1.length()-2);
+			double dblp1=Double.parseDouble(strp1);
+			for(int j=i+1;j<intCount;j++){
+		        String rowData2[]=dataarr[j].split(" - ");
+		    String strp2=rowData2[2];
+			strp2=strp2.substring(0,strp2.length()-2);
+			double dblp2=Double.parseDouble(strp2);
+			if(dblp1<dblp2){
+				String strTemp=dataarr[i];
+	            dataarr[i]=dataarr[j];
+	            dataarr[j]=strTemp;
+	      }
+	  }
+  }
+    for(int i=0;i<intCount;i++){
+		con.println(dataarr[i]);
+	}
+	
+		        
 	}
 		
     public static void highScores(Console con, String strUserName, String strQuizChoice, int intCorrect, int intAmount, double dblPercentage) {
@@ -166,11 +203,16 @@ public static void showAvailableQuizzes(Console con){
         con.println();
         con.println("Score: " + intCorrect + "/" + intAmount + " (" + dblPercentage + "%)");
         con.println();
-        TextOutputFile highScores = new TextOutputFile("HighScores.txt", true);
+        
+    }
+    public static void writeScore(String strUserName, String strQuizChoice, double dblPercentage){
+		TextOutputFile highScores = new TextOutputFile("HighScores.txt", true);
         highScores.println(strUserName + " - " + strQuizChoice + " - " + dblPercentage + "%");
         highScores.close();
-    }
+	}
+		
 }
+
 
 				
 		 
